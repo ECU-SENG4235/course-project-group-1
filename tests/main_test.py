a6d3dfb8778cb93b70d83d673b4f48fbf4b2b938
LuisGVazquez <89490525+LuisGVazquez@users.noreply.github.com>
2024-02-21 06:39:55 -0500

    L2P-33 Created unit tests for audio download functionality and updated video unit test

--- tests/main_test.py
+++ tests/main_test.py
@@ -10,17 +10,17 @@ class TestDownload(unittest.TestCase):
         video_id = "SQL Explained in 100 Seconds"  # Assuming download_video saves the video file with the video_id as name
         download_video(video_url)
         # Check if video file exists and has a non-zero size
-        self.assertTrue(os.path.exists(f"{video_id}.mp4"))
-        self.assertTrue(os.path.getsize(f"{video_id}.mp4") > 0)
+        self.assertTrue(os.path.exists(os.path.join("video_downloads", f"{video_id}.mp4")))
+        self.assertTrue(os.path.getsize(os.path.join("video_downloads", f"{video_id}.mp4")) > 0)
 
-'''    def test_download_audio_high_quality(self):
+    def test_download_audio_high_quality(self):
         # Test successful high quality audio download
         video_url = "https://www.youtube.com/watch?v=zsjvFFKOm3c&ab_channel=Fireship"
         audio_id = "SQL Explained in 100 Seconds"  # Assuming download_audio saves the audio file with the audio_id as name
         download_audio(video_url, 'high')
         # Check if audio file exists and has a non-zero size
-        self.assertTrue(os.path.exists(f"{audio_id}.webm"))
-        self.assertTrue(os.path.getsize(f"{audio_id}.webm") > 0)
+        self.assertTrue(os.path.exists(os.path.join("audio_downloads", f"{audio_id}_high_q.mp3")))
+        self.assertTrue(os.path.getsize(os.path.join("audio_downloads", f"{audio_id}_high_q.mp3")) > 0)
 
     def test_download_audio_low_quality(self):
         # Test successful low quality audio download
@@ -28,10 +28,8 @@ class TestDownload(unittest.TestCase):
         audio_id = "SQL Explained in 100 Seconds"  # Assuming download_audio saves the audio file with the audio_id as name
         download_audio(video_url, 'low')
         # Check if audio file exists and has a non-zero size
-        self.assertTrue(os.path.exists(f"{audio_id}.webm"))
-        self.assertTrue(os.path.getsize(f"{audio_id}.webm") > 0)'''
+        self.assertTrue(os.path.exists(os.path.join("audio_downloads", f"{audio_id}_low_q.mp3")))
+        self.assertTrue(os.path.getsize(os.path.join("audio_downloads", f"{audio_id}_low_q.mp3")) > 0)
 
 if __name__ == '__main__':
-    unittest.main()     
-    
-    '''Still need to download actual audio'''
+    unittest.main()
