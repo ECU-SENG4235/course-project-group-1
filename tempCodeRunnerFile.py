d9fbcf0bd2eac1eac9a0f0828f93f6165a0205a7
srgevrg <kendalll1022@gmail.com>
2024-02-26 13:02:40 -0500

    rouning edges

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -11,36 +11,11 @@ def download():
     elif choice == 'V':
         download_video(video_url)
     else:
-        messagebox.showerror("Error", "Invalid choice. Please select either Audio or Videx  o.")
+        messagebox.showerror("Error", "Invalid choice. Please select either Audio or Video.")
 
 # Initialize the main window
 root = tk.Tk()
 root.title("Link2Playback")
-
-#rounded edges
-def create_rounded_rectangle(canvas, x1, y1, x2, y2, radius, **kwargs):
-    points = [x1 + radius, y1,
-              x1 + radius, y1,
-              x2 - radius, y1,
-              x2 - radius, y1,
-              x2, y1,
-              x2, y1 + radius,
-              x2, y1 + radius,
-              x2, y2 - radius,
-              x2, y2 - radius,
-              x2, y2,
-              x2 - radius, y2,
-              x2 - radius, y2,
-              x1 + radius, y2,
-              x1 + radius, y2,
-              x1, y2,
-              x1, y2 - radius,
-              x1, y2 - radius,
-              x1, y1 + radius,
-              x1, y1 + radius,
-              x1, y1]
-
-    return canvas.create_polygon(points, **kwargs, smooth=True)
 
 # Set initial window size
 root.geometry("736x414")
